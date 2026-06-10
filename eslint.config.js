b08d438a612841f2eb6bcc345607ab156a6cec59
Khonsu_144 <pstest451@gmail.com>
2026-06-10 11:33:45 +0300

    test

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2024,
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/brace-style': ['error', '1tbs'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
+      '@stylistic/eol-last': ['error', 'always'],
+      'no-console': 'off',
+    },
+  },
+];
